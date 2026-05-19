ef281b3ef1f90617f2f323f4ff9bd91eddbae44d
Danieldrj6 <macarenadrj@hotmail.com>
2026-05-19 10:17:51 -0400

    correccion ventas

--- integrantes/daniel/ms_ventas/src/main/java/com/ventas/example/ms_ventas/repository/VentasRepositorio.java
+++ integrantes/daniel/ms_ventas/src/main/java/com/ventas/example/ms_ventas/repository/VentasRepositorio.java
@@ -4,8 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ventas.example.ms_ventas.model.VentasModelo;
+import java.util.List;
 
 @Repository
 public interface VentasRepositorio extends JpaRepository<VentasModelo, Integer> {
+
+    List<VentasModelo> findByCarrito_id(Integer carrito_id);
+
+    List<VentasModelo> findByUser_id(Integer user_id);
+
+    List<VentasModelo> findByEs_factura(boolean es_factura);
+
+    List<VentasModelo> findAll();
 
 }
