d82b7cf075f571eafca38500ca8b0cbf83f9df8e
Danieldrj6 <macarenadrj@hotmail.com>
2026-06-04 11:25:27 -0400

    Agrego FakerLoader basico

---
+++ integrantes/daniel/ms_carrito20/src/main/java/com/ms_carrito/example/ms_carrito20/DataLoader.java
@@ -0,0 +1,45 @@
+package com.ms_carrito.example.ms_carrito20;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.ms_carrito.example.ms_carrito20.model.Carrito;
+import com.ms_carrito.example.ms_carrito20.repository.RepositoryCarrito;
+import com.ms_carrito.example.ms_carrito20.repository.RepositoryItemsCarrito;
+
+import net.datafaker.Faker;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+@Profile("dev")
+@Component
+public class DataLoader implements CommandLineRunner{
+
+    @Autowired
+    private RepositoryCarrito repositoryCarrito;
+    @Autowired
+    private RepositoryItemsCarrito repositoryItemsCarrito;
+
+    @Override
+    public void run(String... args) throws Exception{
+        Faker faker = new Faker();
+        Random random = new Random();
+
+        // generar carrito
+        for (int i = 2; i < 10; i++){
+            Carrito carrito = new Carrito();
+            carrito.setUserId(i + 1);
+            carrito.setTotalPrecio(faker.number().numberBetween(5000, 500000));
+            repositoryCarrito.save(carrito); 
+        }
+
+        List<Carrito> carritos = repositoryCarrito.findAll();
+
+
+    }
+
+}
--- integrantes/daniel/ms_carrito20/src/main/java/com/ms_carrito/example/ms_carrito20/repository/RepositoryCarrito.java
+++ integrantes/daniel/ms_carrito20/src/main/java/com/ms_carrito/example/ms_carrito20/repository/RepositoryCarrito.java
@@ -4,8 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ms_carrito.example.ms_carrito20.model.Carrito;
+import java.util.List;
 
 @Repository
 public interface RepositoryCarrito extends JpaRepository<Carrito, Integer> {
+
+    List<Carrito>findAll();
+
+    Carrito save(Carrito carrito);
 
 }
